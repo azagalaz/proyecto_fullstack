62dc86bde24e03375974e558864b9fe8eb34b053
alberto <azagalaz@ieshlanz.es>
2025-02-13 11:13:57 +0100

    cambiado .env

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -f /etc/nginx/certs/selfsigned.crt ]; then
     echo "Generando certificado autofirmado..."
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -subj "/C=US/ST=Estado/L=Ciudad/O=Organizacion/OU=Departamento/CN=localhost_IP_Dominio/emailAddress=admin@miempresa.com" \
+        -subj "/C=ES/ST=Granada/L=Granada/O=IES HLANZ/OU=DAW/CN=localhost/emailAddress=alberto@miempresa.com" \
         -keyout /etc/nginx/certs/selfsigned.key \
         -out /etc/nginx/certs/selfsigned.crt
 fi
